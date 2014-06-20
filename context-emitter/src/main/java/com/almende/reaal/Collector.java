0cf48762c83b2074772017891b65b0174b678239
Ludo Stellingwerff <ludo@almende.org>
2014-06-20 23:04:25 +0200

    Updated to newest Eve 3.0 code

--- context-emitter/src/main/java/com/almende/reaal/Collector.java
+++ context-emitter/src/main/java/com/almende/reaal/Collector.java
@@ -22,8 +22,8 @@ import org.universAAL.middleware.owl.MergedRestriction;
 
 import com.almende.eve.capabilities.handler.SimpleHandler;
 import com.almende.eve.scheduling.Scheduler;
+import com.almende.eve.scheduling.SimpleSchedulerBuilder;
 import com.almende.eve.scheduling.SimpleSchedulerConfig;
-import com.almende.eve.scheduling.SimpleSchedulerFactory;
 import com.almende.eve.transport.Receiver;
 import com.almende.util.jackson.JOM;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -33,11 +33,17 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 /**
  * The Class Collector.
  */
-public class Collector implements Receiver{
-	private ContextPublisher	cpublisher	= null;
+public class Collector implements Receiver {
+	private ContextPublisher		cpublisher	= null;
 	
-	private Scheduler scheduler = SimpleSchedulerFactory.getScheduler(new SimpleSchedulerConfig(), new SimpleHandler<Receiver>(this));
-	private HashMap<String,Double> lastVals = new HashMap<String,Double>();
+	private Scheduler				scheduler	= new SimpleSchedulerBuilder()
+														.withConfig(
+																new SimpleSchedulerConfig())
+														.withHandle(
+																new SimpleHandler<Receiver>(
+																		this))
+														.build();
+	private HashMap<String, Double>	lastVals	= new HashMap<String, Double>();
 	
 	/**
 	 * Instantiates a new collector.
@@ -59,13 +65,15 @@ public class Collector implements Receiver{
 		myContextProvider
 				.setProvidedEvents(new ContextEventPattern[] { myContextEventPattern });
 		cpublisher = new DefaultContextPublisher(context, myContextProvider);
-		scheduler.schedule("Go!",DateTime.now().plus(1000));
+		scheduler.schedule("Go!", DateTime.now().plus(1000));
+		scheduler.schedule("Go!", DateTime.now().plus(35000));
+		scheduler.schedule("Go!", DateTime.now().plus(55000));
 	}
 	
 	/**
 	 * Stop.
 	 */
-	public void stop(){
+	public void stop() {
 		cpublisher.close();
 	}
 	
@@ -75,34 +83,43 @@ public class Collector implements Receiver{
 	 * @param sensorId
 	 *            the sensor id
 	 */
-	public void getSensorData(String sensorId){
+	public void getSensorData(String sensorId) {
 		AccountBean account = new AccountBean();
 		account.setEmail("reaaltest@almende.org");
 		account.setPassword("DossierRijnmond15");
 		SenseClient.login(account);
 		
 		String result = SenseClient.getSensorValue(sensorId);
-		System.err.println("Result received:"+result);
+		System.err.println("Result received:" + result);
 		try {
 			JsonNode tree = JOM.getInstance().readTree(result);
 			ArrayNode data = (ArrayNode) tree.get("data");
-			ArrayNode goals = (ArrayNode)JOM.getInstance().readTree(data.get(0).get("value").asText());
+			ArrayNode goals = (ArrayNode) JOM.getInstance().readTree(
+					data.get(0).get("value").asText());
+			System.err.println("Goals:" + goals.size());
 			Iterator<JsonNode> iter = goals.elements();
-			while (iter.hasNext()){
+			while (iter.hasNext()) {
 				JsonNode node = iter.next();
-				String id = sensorId+"_"+node.get("id").asInt();
+				String id = sensorId + "_" + node.get("id").asInt();
 				Double lastVal = lastVals.get(id);
-				if (lastVal != null && lastVal >= data.get("date").asDouble()){
-					//Ignore this value;
+				System.err.println("Checking :" + lastVal + " with:"
+						+ data.get(0).get("date").asDouble());
+				if (lastVal != null
+						&& lastVal >= data.get(0).get("date").asDouble()) {
+					// Ignore this value;
 				} else {
-					lastVals.put(id, node.get("progress").asDouble());
-					publish(id,new Measurement(node.get("progress").asDouble()));
+					System.err.println("Publishing!");
+					lastVals.put(id, data.get(0).get("date").asDouble());
+					publish(id,
+							new Measurement(node.get("progress").asDouble()));
 				}
 			}
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (Throwable t) {
+			t.printStackTrace();
 		}
 		
 		return;
@@ -131,11 +148,12 @@ public class Collector implements Receiver{
 			e.printStackTrace();
 		}
 	}
-
+	
 	@Override
 	public void receive(Object msg, URI senderUrl, String tag) {
-		//Scheduler receival (check if other transports are added in the future.
+		// Scheduler receival (check if other transports are added in the
+		// future.
+		scheduler.schedule("Go!", DateTime.now().plus(10000));
 		getSensorData("540641");
-		scheduler.schedule("Go!",DateTime.now().plus(10000));
 	}
 }
